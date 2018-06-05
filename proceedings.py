524056d53e4a1999378e392eddf8e8a440df9b74
kavin2606 <kavin2@pdx.edu>
2018-06-05 11:53:27 -0700

    query-4 logic updated

--- proceedings.py
+++ proceedings.py
@@ -1,22 +1,20 @@
-all_proc=connection.execute(select proc_id,author_ids from proceedings)
-#storing temporary maximum,and their ids
+from cassandra.cluster import Cluster
+cluster = Cluster()
+session = cluster.connect('dblp')
+all_procs=session.execute('select proc_id,auth_ids from procd where year=2005 allow filtering')
 temp_max=0
 proc_list=[]
-#count the size of the author_ids array and find the publications with maximum count
-for proc in all_proc{
-	if(len(proc.author_ids)>temp_max){
-		proc_list.clear()
-		proc_list.append(proc.proc_id)
-		temp_max=len(proc.author_ids)
-	}
-#handles the case where two proceedings has the maximum count
-	else if(len(proc.author_ids)=temp_max){
-		proc_list.append(proc.proc_id)
-	}
-}
-#for each of the proc_id print its details
-for i in proc_list
-{
-	result=connection.execute(select proc_id,title from proceedings where proc_id=%i)
-	print result
-}
+for proc in all_procs:
+    if(len(proc.auth_ids)>temp_max):
+        proc_list=[]
+        proc_list.append(proc.proc_id)
+        temp_max=len(proc.auth_ids)
+    elif(len(proc.auth_ids)== temp_max):
+        proc_list.append(proc.proc_id)
+
+for j in proc_list:
+    res1= session.execute('select proc_id,pub_type,title,year,conference from procd where proc_id = %s',(j,))
+for res2 in res1:
+    print res2
+
+
